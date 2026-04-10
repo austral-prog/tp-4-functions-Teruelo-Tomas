ba7d34a4710b65fa0118f01593809e572f75684c
Teruelo-Tomas <tteruelo@mail.austral.edu.ar>
2026-04-10 14:25:01 -0300

    Add files via upload

--- classify_number.py
+++ classify_number.py
@@ -22,4 +22,6 @@ def classify_number(n):
       - "negative odd"    (negativo e impar)
       - "zero"            (el número es 0)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if is_even(n) and is_positive(n):
+        return 'positivo'
+
